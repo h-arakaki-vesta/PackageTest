ae7b5b49c0ca4ff1e1c4e5ccac56d3a5ceaa9739
h-arakaki-vesta <h-arakaki@vesta-ltd.co.jp>
2024-02-06 18:18:50 +0900

    Unity Package Maangerを自作テスト

---
+++ Scripts/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ArakakiTest
+{
+    public static class HelloWorld
+    {
+        public static string GetMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
